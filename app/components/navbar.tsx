d4dbe6dd99a14a62e7bf84851d6f09857fa6c4c4
muhsin-life <muhsin.m@life-me.com>
2023-05-16 16:09:17 +0400

    commit

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -612,7 +612,7 @@ const Navbar = ({ data, brands_data, sessionServ, isArabic, lang, langData, lang
                     </svg>
                   </div>
                   {SearchLoadingState ?
-                    <svg fill="none" className="animate-spin md:block hidden" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" shape-rendering="geometricPrecision" viewBox="0 0 24 24" height="24" width="24" ><path d="M12 2v4M12 18v4M4.93 4.93l2.83 2.83M16.24 16.24l2.83 2.83M2 12h4M18 12h4M4.93 19.07l2.83-2.83M16.24 7.76l2.83-2.83"></path></svg> : ""}
+                    <svg fill="none" className={`animate-spin w-5 h-5 absolute inline ${isArabic ? "left-8" : "right-8"}  inset-y-0 m-auto w-4 h-4 mx-2`} stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" shape-rendering="geometricPrecision" viewBox="0 0 24 24" height="24" width="24" ><path d="M12 2v4M12 18v4M4.93 4.93l2.83 2.83M16.24 16.24l2.83 2.83M2 12h4M18 12h4M4.93 19.07l2.83-2.83M16.24 7.76l2.83-2.83"></path></svg> : ""}
 
 
 
